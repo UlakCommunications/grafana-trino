9b7ad4b11bfddb1fd000c3c774245c7029136758
fatih.yuce <fatih.yuce@ulakhaberlesme.com.tr>
2024-08-27 16:01:38 +0300

    removes

--- push.sh
+++ push.sh
@@ -1,10 +1,12 @@
- VERSION=$1
- set -x
+set -x
+set -e
 
 rm -rf ./dist
+rm -rf ./trino-datasource
 yarn build --skipTest
 mage -v
-cp -r ./dist trino-datasource
+cp -r ./dist ./trino-datasource
 zip -r trino-datasource.zip trino-datasource
 curl -v -u admin:nexusulak2022 --upload-file ./trino-datasource.zip http://192.168.57.202:8081/repository/file/trino-datasource.zip
+rm -rf ./trino-datasource
 
